781a8963906fd4cba1f5007614681b2953017023
Anton-Kovalevskiy <kaval1986@mail.ru>
2023-06-01 00:29:19 +0300

    Решение задач № 50 и №52

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ switch (task)
     Task47 (task);
     break;
     case "50":
-  //  Task50 (task);
+    Task50 (task);
     break;
     case "52":
- //   Task52 (task);
+    Task52 (task);
     break;
     default:
     break;
@@ -38,7 +38,7 @@ int size (string message)
     return (s);
 }
 
-double [,] FillPrintArray (int m, int n)
+double [,] FillPrintDoubleArray (int m, int n)
 {
     double [,] array = new double [m, n]; 
     Console.WriteLine($"Получен массив {m}x{n}:");
@@ -61,6 +61,110 @@ string mes = "Введите размер m: ";
 int m = size (mes);
 mes = "Введите размер n: ";
 int n = size (mes);
-double [,] array = FillPrintArray(m, n);
+double [,] array = FillPrintDoubleArray(m, n);
 }
 
+/*
+Задача 50. Напишите программу, 
+которая на вход принимает значение элемента в двумерном массиве, 
+и возвращает позицию этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+
+*/
+
+// Решение
+
+int [,] FillPrintIntArray (int m, int n)
+{
+    int [,] array = new int [m, n]; 
+    Console.WriteLine($"Получен массив {m}x{n}:");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array [i, j] = new Random().Next(0, 10);
+
+            Console.Write($"[{array[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+    return (array);
+}
+
+string FindNumberInArray (int [,] array, int f)
+{
+    string result = String.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array [i, j] == f)
+            {
+                result = $"Искомый элемент {f} находится в массиве на позиции [{i}; {j}]";
+                break;
+            }          
+        }
+    }
+    if (result == String.Empty) result = $"Искомый элемент {f} в массиве отсутствует!";
+    return (result);
+}
+
+void Task50(string task)
+{
+string mes = "Введите размер m: ";
+int m = size (mes);
+mes = "Введите размер n: ";
+int n = size (mes);
+int [,] array = FillPrintIntArray(m, n);
+mes = "Введите искомый элемент массива: ";
+int findNumber = size (mes);
+string position = FindNumberInArray(array, findNumber);
+Console.Write(position);
+}
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+double [] FindAverageColumns (int [,] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    double [] average = new double [array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            average [i] += array [j, i];
+            if (j == array.GetLength(0) - 1) average [i] /= array.GetLength(0);
+        }
+        Console.Write($"[{Math.Round(average [i], 1)}] ");
+    }
+    return (average);
+}
+
+void Task52 (string task)
+{
+    string mes = "Введите размер m: ";
+    int m = size (mes);
+    mes = "Введите размер n: ";
+    int n = size (mes);
+    int [,] array = FillPrintIntArray(m, n);
+    double [] average = FindAverageColumns(array);
+}
